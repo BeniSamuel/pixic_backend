4648322c47bc616f4fc0b64b452cac9027a6cc7c
benisamuel <benisamuel566@gmail.com>
2025-05-29 17:19:03 +0200

    added user controller, module and service

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,45 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Put } from "@nestjs/common";
+import { UserService } from "./user.service";
+import { User } from "src/entities/user.entity";
+import { ResponseEntity } from "src/util/response.util";
+import { UserUpdateDto } from "src/dto/updateUser.dto";
 
 @Controller("/api/pixic/v1/user")
 export class UserController {
+    constructor (private readonly userService: UserService) {}
 
+    @Get("/all")
+    @HttpCode(HttpStatus.OK)
+    async getAllUsers (): Promise<ResponseEntity<User[]>> {
+        return ResponseEntity.ok("Successfully obtained all users!!! 🎉🎉🎉", await this.userService.getAllUser());
+    }
+
+    @Get("/:id")
+    @HttpCode(HttpStatus.OK | HttpStatus.NOT_FOUND)
+    async getUserById (@Param("id") id: number): Promise<ResponseEntity<User>> {
+        const user: User = await this.userService.getUserById(id);
+        if (user) {
+            return ResponseEntity.ok("Successfully obtained user!!! 🎉🎉🎉", user);
+        }
+        return ResponseEntity.notFound("Failed to obtain user!!! 😔💔💔", null);
+    }
+
+    @Put("/update/:id")
+    @HttpCode(HttpStatus.OK | HttpStatus.NOT_FOUND)
+    async updateUserById (@Param("id") id: number, @Body() updateUserDto: UserUpdateDto): Promise<ResponseEntity<User>> {
+        const user: User = await this.userService.updateUser(id, updateUserDto);
+        if (user) {
+            return ResponseEntity.ok("Successfully updated user!!! 🎉🎉🎉", user);
+        }
+        return ResponseEntity.notFound("Failed to update user!!! 😔💔💔", null);
+
+    }
+
+    @Delete("/delete/:id")
+    @HttpCode(HttpStatus.OK | HttpStatus.NOT_FOUND)
+    async deleteUserById (@Param("id") id: number): Promise<ResponseEntity<Boolean>> {
+        return this.userService.deleteUserById(id) ?
+        ResponseEntity.ok("Successfully deleted user!!! 🎉🎉🎉", true) :
+        ResponseEntity.notFound("Failed to delete user!!! 😔💔💔", null);
+    }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,10 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable, NotFoundException, UnauthorizedException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
+import { RegisterDto } from "src/dto/register.dto";
 import { User } from "src/entities/user.entity";
 import { Repository } from "typeorm";
+import * as bcrypt from "bcrypt";
+import { UserUpdateDto } from "src/dto/updateUser.dto";
 
 @Injectable()
 export class UserService {
@@ -13,6 +16,47 @@ export class UserService {
     }
 
     getUserById (id: number): Promise<User | null> {
-        return this.userRepository.findOneBy({ id });
+        return this.userRepository.findOne({where: {id}});
+    }
+
+    getUserByEmail (email: string): Promise<User | null> {
+        return this.userRepository.findOne({where: {email}});
+    } 
+
+    async createUser (registerDto: RegisterDto): Promise<User> {
+        const user = this.getUserByEmail(registerDto.getEmail());
+        if (user) { 
+            throw new BadRequestException("User already exist stupid!!! 😔💔💔"); 
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(registerDto.getPassword(), salt);
+        const newUser = new User(registerDto.getName(), registerDto.getEmail(), hashedPassword, registerDto.getRole());
+
+        return this.userRepository.save(newUser);
+    }
+
+    async updateUser (id: number, updateUserDto: UserUpdateDto): Promise<User> {
+        const user = await this.getUserById(id);
+        if (!user) {
+            return null;
+        }
+
+        user.name = updateUserDto.getName();
+        user.email = updateUserDto.getEmail();
+        user.password = await bcrypt.hash(updateUserDto.getPassword(), 10);
+        user.phone = updateUserDto.getPhone();
+        user.role = updateUserDto.getRole();
+
+        return this.userRepository.save(user);
+    }
+
+    async deleteUserById (id: number): Promise<Boolean> {
+        const user = await this.getUserById(id);
+        if (user) {
+            this.userRepository.delete(user);
+            return true;
+        }
+        return false;
     }
 }
