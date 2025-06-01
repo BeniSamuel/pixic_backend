4bd304f944644ea653f1405c78dd202dd348c4ac
benisamuel <benisamuel566@gmail.com>
2025-06-01 13:26:27 +0200

    finalized booking service

--- src/booking/booking.service.ts
+++ src/booking/booking.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
+import { BookingDto } from "src/dto/booking.dto";
 import { Booking } from "src/entities/booking.entity";
 import { User } from "src/entities/user.entity";
+import { Status } from "src/enums/status.enum";
 import { UserService } from "src/user/user.service";
 import { Repository } from "typeorm";
 
@@ -43,7 +45,51 @@ export class BookingService {
         }
     } 
 
-    async createBooking (): Promise<Booking> {
-        const user: User = await this.userService.get
+    async createBooking (bookingDto: BookingDto): Promise<Booking> {
+        const user: User = await this.userService.getUserById(bookingDto.user_id);
+        if (!user) {
+            return null;
+        }
+
+        const booker: User = await this.userService.getUserById(bookingDto.booker_id);
+        if (!booker) {
+            return null;
+        }
+
+        const newBooking: Booking = new Booking(user, booker, bookingDto.details, bookingDto.bookingDate, Status.PENDING);
+        return this.bookingRepository.save(newBooking);
+    }
+
+    async updateBookingById (id: number, bookingDto: BookingDto): Promise<Booking> {
+        const booking: Booking = await this.getBookingById(id);
+        if (!booking) {
+            return null;
+        }
+
+        const user: User = await this.userService.getUserById(bookingDto.user_id);
+        if (!user) {
+            return null;
+        }
+
+        const booker: User = await this.userService.getUserById(bookingDto.booker_id);
+        if (!booker) {
+            return null;
+        }
+
+        booking.user = user;
+        booking.booker = booker;
+        booking.details = bookingDto.details;
+        booking.bookingDate = bookingDto.bookingDate;
+        
+        return this.bookingRepository.save(booking);
+    }
+
+    async deleteBookingById (id: number): Promise<Boolean> {
+        const booking: Booking = await this.getBookingById(id);
+        if (booking) {
+            this.bookingRepository.delete(booking);
+            return true;
+        }
+        return false;
     }
 }
